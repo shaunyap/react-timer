db470cdd79879bbdda8334d8b8ddc28522fd7e38
Shaun Yap <shaun@siegeflow.com>
2016-06-09 22:00:37 +0400

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+var Countdown = () => {
+    return (
+        <div>
+            <h1 className='text-center page-title'>Countdown</h1>
+        </div>
+    );
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+var Timer = () => {
+    return (
+        <div>
+            <h1 className='text-center page-title'>Timer</h1>
+        </div>
+    );
+}
+
+module.exports = Timer;
