7b3b2acfb8168d09b2bcbcf1339e507616073bd7
Shaun Yap <shaun@siegeflow.com>
2016-06-12 13:14:06 +0400

    Update status to stop when count reaches 0

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,4 +1,4 @@
-var React = require('react');
+  var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -10,27 +10,37 @@ var Countdown = React.createClass({
             countdownStatus: 'stopped'
         };
     },
-componentDidUpdate: function(prevProps, prevState) {
-    if (this.state.countdownStatus !== prevState.countdownStatus) {
-        switch (this.state.countdownStatus) {
-            case 'started':
-              this.startTimer();
-              break;
-            case 'stopped':
-              this.setState({count: 0})
-            case 'paused':
-              clearInterval(this.timer)
-              this.timer = undefined;
-              break;
+    componentDidUpdate: function(prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
+                case 'started':
+                  this.startTimer();
+                  break;
+                case 'stopped':
+                  this.setState({count: 0})
+                case 'paused':
+                  clearInterval(this.timer)
+                  this.timer = undefined;
+                  break;
+            }
         }
-    }
-},
+    },
+
+    componentWillUnmount: function() {
+      clearInterval(this.timer);
+      this.timer = undefined;
+    },
+
     startTimer: function () {
       this.timer = setInterval(() => {
         var newCount = this.state.count - 1;
         this.setState({
           count: newCount >= 0 ? newCount : 0
         });
+
+        if(newCount === 0) {
+          this.setState({countdownStatus: 'stopped'})
+        }
       }, 1000);
     },
     handleSetCountdown: function (seconds) {
